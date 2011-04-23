28d80e3f19c8cec52b0743820233886eb828857d
Rodrigo Ayala <rayalac@yahoo.com>
2011-04-23 17:26:35 -0300

    Se agregan archivos RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RendimientoFisico::Application.config.secret_token = '8ec6c06286e18bcf65551286597e2091cef06a10885340a2b5e844a5ac748e04690f9ba9e19596b23ddb4730466749045201d6c3606d66ea1192e731291f9fb1'
